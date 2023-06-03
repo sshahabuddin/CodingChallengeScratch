d631241807ad041ddd174d55aefa9f2d500ad52a
sshahabuddin <sts.shahabuddin@gmail.com>
2023-06-03 09:56:14 -0700

    Update CodeChallengeScratch.js
    Added comments, and fixed some errors

--- CodeChallengeScratch.js
+++ CodeChallengeScratch.js
@@ -1,23 +1,27 @@
+//Used to get the user's input
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+//async function to fetch the endpoint
+//Paramaters are the user's input to search the JSON and the url
 async function loadList(url, key) {
   const res = await fetch(url);
-
+	
+  //If there is an error with the connection an error is thrown	
   if (!res.ok) {
 		throw new Error(`HTTP error! status: ${res.status}`);
 	}
 
-	const data = await res.json();
-  //console.log(res);
-  //console.log(data);
-  //return data;
+  const data = await res.json();
+
+  //Search the JSON object with the user's input 	
   search(data, key);
 
 }
 
+//The search function for the JSON object
 function search(data, key){
 if (typeof data !== "object" || data === null) {
       return data === key ? data : undefined;
@@ -31,6 +35,7 @@ if (typeof data !== "object" || data === null) {
   }
 }
 
+//Asks the user for their input 
 readline.question('Enter Clinic Name\n', name => {
   const clinicURL = "https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json";
 
